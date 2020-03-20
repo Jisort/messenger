29094079c6213ce182138311aad1e59d593f2848
willy <willymwaid92@gmail.com>
2020-03-20 18:01:11 +0300

    update -- sms topup

--- src/functions/baseUrls.js
+++ src/functions/baseUrls.js
@@ -1,8 +1,7 @@
 module.exports = {
     serverBaseUrl: function () {
         if(process.env.NODE_ENV === 'development') {
-            // return 'http://localhost:8000';
-            return 'https://my.jisort.com';
+            return 'http://localhost:8000';
         } else {
             return 'https://my.jisort.com';
         }
